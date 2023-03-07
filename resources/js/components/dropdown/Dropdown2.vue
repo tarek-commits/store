a80b85aa7acd6f0a2b97dfb57d5887008b753177
bahaa26 <bahaaazoz398@gmail.com>
2023-03-07 21:27:30 +0200

    ts init and fix build problems

--- resources/js/components/dropdown/Dropdown2.vue
+++ resources/js/components/dropdown/Dropdown2.vue
@@ -1,73 +1,97 @@
 <template>
-    <!--begin::Menu 2-->
-    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semobold w-200px"
-        data-kt-menu="true">
+  <!--begin::Menu 2-->
+  <div
+    class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semobold w-200px"
+    data-kt-menu="true"
+  >
+    <!--begin::Menu item-->
+    <div class="menu-item px-3">
+      <div class="menu-content fs-6 text-dark fw-bold px-3 py-4">
+        Quick Actions
+      </div>
+    </div>
+    <!--end::Menu item-->
+
+    <!--begin::Menu separator-->
+    <div class="separator mb-3 opacity-75"></div>
+    <!--end::Menu separator-->
+
+    <!--begin::Menu item-->
+    <div class="menu-item px-3">
+      <a href="#" class="menu-link px-3"> New Ticket </a>
+    </div>
+    <!--end::Menu item-->
+
+    <!--begin::Menu item-->
+    <div class="menu-item px-3">
+      <a href="#" class="menu-link px-3"> New Customer </a>
+    </div>
+    <!--end::Menu item-->
+
+    <!--begin::Menu item-->
+    <div
+      class="menu-item px-3"
+      data-kt-menu-trigger="hover"
+      data-kt-menu-placement="right-start"
+      data-kt-menu-flip="left-start, top"
+    >
+      <!--begin::Menu item-->
+      <a href="#" class="menu-link px-3">
+        <span class="menu-title">New Group</span>
+        <span class="menu-arrow"></span>
+      </a>
+      <!--end::Menu item-->
+
+      <!--begin::Menu sub-->
+      <div class="menu-sub menu-sub-dropdown w-175px py-4">
         <!--begin::Menu item-->
         <div class="menu-item px-3">
-            <div class="menu-content fs-6 text-dark fw-bold px-3 py-4">
-                Quick Actions
-            </div>
+          <a href="#" class="menu-link px-3"> Admin Group </a>
         </div>
         <!--end::Menu item-->
-
-        <!--begin::Menu separator-->
-        <div class="separator mb-3 opacity-75"></div>
-        <!--end::Menu separator-->
 
         <!--begin::Menu item-->
-        <div class="menu-item px-3" v-for="(title, index) in labels">
-            <div class="form-check form-check-sm form-check-custom form-check-solid">
-                <a class="menu-link px-3"> <input class="form-check-input" @change="toggleLabel(index)" :checked="title.show" type="checkbox" /> <label for="">{{ title.title }}</label> </a>
-            </div>
+        <div class="menu-item px-3">
+          <a href="#" class="menu-link px-3"> Staff Group </a>
         </div>
         <!--end::Menu item-->
 
-
-        <!--begin::Menu separator-->
-        <div class="separator mt-3 opacity-75"></div>
-        <!--end::Menu separator-->
-
+        <!--begin::Menu item-->
+        <div class="menu-item px-3">
+          <a href="#" class="menu-link px-3"> Member Group </a>
+        </div>
+        <!--end::Menu item-->
+      </div>
+      <!--end::Menu sub-->
     </div>
-    <!--end::Menu 2-->
+    <!--end::Menu item-->
+
+    <!--begin::Menu item-->
+    <div class="menu-item px-3">
+      <a href="#" class="menu-link px-3"> New Contact </a>
+    </div>
+    <!--end::Menu item-->
+
+    <!--begin::Menu separator-->
+    <div class="separator mt-3 opacity-75"></div>
+    <!--end::Menu separator-->
+
+    <!--begin::Menu item-->
+    <div class="menu-item px-3">
+      <div class="menu-content px-3 py-3">
+        <a class="btn btn-primary btn-sm px-4" href="#"> Generate Reports </a>
+      </div>
+    </div>
+    <!--end::Menu item-->
+  </div>
+  <!--end::Menu 2-->
 </template>
 
 <script lang="ts">
-import { defineComponent, toRef, computed } from "vue";
-import { storeToRefs } from 'pinia'
-import { useTablesData } from "../../stores/tablesData";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-    name: "check-items",
-    components: {},
-    props: {
-        enumName: String
-    },
-
-    setup(props) {
-        const tablesData = useTablesData();
-        const { getLables } = storeToRefs(tablesData);
-        const labels = computed(() => tablesData.getLables(enumName.value));
-        const enumName = toRef(props, 'enumName');
-        function toggleLabel(index) {
-            tablesData.toggleLabel(enumName.value, index);
-        }
-        return {
-            tablesData,
-            getLables,
-            enumName,
-            toggleLabel,
-            labels
-        }
-    }
+  name: "dropdown-2",
+  components: {},
 });
 </script>
-
-<style lang="scss">
-    .menu {
-        .menu-item {
-            label {
-                margin-left: 10px;
-            }
-        }
-    }
-</style>
